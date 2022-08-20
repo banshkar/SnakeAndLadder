b65925433a0e43584e2a5ac292881fc4ac7abaa0
jiendra <jitendra@banshkar.com>
2022-08-20 11:16:34 +0530

    add new varible Numberofplayer_1 and 2

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,6 +3,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.Scanner;
+/*All UC- Used There And All UC -Merge
+FULl Game completly*/
 class SnakeAndLadder1 {
     final static int WinPoint = 100;
     static Map<Integer, Integer> snake = new HashMap<>();
@@ -10,18 +12,19 @@ class SnakeAndLadder1 {
     {
         // Store key and position
         // Used 5 snake
-        snake.put(90, 50);
-        snake.put(81, 60);
-        snake.put(45, 24);
-        snake.put(35, 15);
-        snake.put(75, 20);
-        // Used 5 Ladder
-        Ladder.put(6, 15);
-        Ladder.put(30, 53);
-        Ladder.put(60, 83);
-        Ladder.put(25, 90);
-        Ladder.put(12, 70);
+        snake.put(99,54);
+        snake.put(70,55);
+        snake.put(52,42);
+        snake.put(25,2);
+        snake.put(95,72);
+
+        Ladder.put(6,25);
+        Ladder.put(11,40);
+        Ladder.put(60,85);
+        Ladder.put(46,90);
+        Ladder.put(17,69);
     }
+    // UC-2 There Used
     public int dice() {
         int start = 0;
         Random r = new Random();
@@ -53,17 +56,23 @@ class SnakeAndLadder1 {
         Scanner sc = new Scanner(System.in);
         String pressed;
         int Dice = 0;
+        int used_numberofdice_player_1=0;
+        int used_numberofdice_player_2=0;
         do {
+            // UC-1 There Used
             System.out.println((currentposition == -1 ?
                     "\n\n First players turn" : "\n\nsecond players turn"));
                 System.out.println(" Press  's' ");
             pressed = sc.next();
             Dice = dice();
+            // UC-3 There Used
             if (currentposition == -1) {
                 Play_1_position = TotalValue(Play_1_position, Dice);
                 System.out.println("Player_1_ is postion :" + Play_1_position);
                 System.out.println("Player_2_ is postion :" + Play_2_position);
+                used_numberofdice_player_1++;
                 if (isWin(Play_1_position)) {
+                    System.out.println("total number used dice" + used_numberofdice_player_1);
                     System.out.println("Congratulations! First player won");
                     return;
                 }
@@ -71,7 +80,9 @@ class SnakeAndLadder1 {
                 Play_2_position = TotalValue(Play_2_position, Dice);
                 System.out.println("Player_1_ is postion :" + Play_1_position);
                 System.out.println("Player_2_ is postion :" + Play_2_position);
+                used_numberofdice_player_2++;
                 if (isWin(Play_1_position)) {
+                    System.out.println("total number used dice" + used_numberofdice_player_2);
                     System.out.println("Congratulations! Second player won");
                     return;
                 }
